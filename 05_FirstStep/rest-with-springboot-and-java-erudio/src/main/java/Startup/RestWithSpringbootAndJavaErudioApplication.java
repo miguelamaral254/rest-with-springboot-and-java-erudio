b231dec9cecf53b2a30dd62f9794001d009955cd
miguelamaral254 <miguel.amaral.sales@gmail.com>
2024-04-10 19:42:46 -0300

    create springboot

---
+++ 05_FirstStep/rest-with-springboot-and-java-erudio/src/main/java/Startup/RestWithSpringbootAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package Startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootAndJavaErudioApplication.class, args);
+	}
+
+}
